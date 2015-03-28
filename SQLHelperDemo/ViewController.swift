8d0e7b648207b489803ba736d0a095278b03b799
Ahmed Hamdy <dimo.hamdy@gmail.com>
2015-03-28 23:53:27 +0200

    Initial Commit

---
+++ SQLHelperDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQLHelperDemo
+//
+//  Created by binaryboy on 3/28/15.
+//  Copyright (c) 2015 AhmedHamdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
